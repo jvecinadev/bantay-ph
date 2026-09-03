7a5fe172b5ff431788917bcbc7ab433449c8e59e
VCNtech-123 <johnvecina640@gmail.com>
2026-09-04 05:05:32 +0800

    feat: reorganize middleware setup and ensure trust proxy is set before CORS

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,19 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import { errorHandler } from './middleware/errorHandler.middleware'
 
 const app = express()
 
 app.use(express.json())
+app.set("trust proxy", 1)
 app.use(cors({
         origin: 'http://localhost:5173',
         credentials: true
 }))
 app.use(cookieParser())
-app.set("trust proxy", 1)
+
+
+app.use(errorHandler)
 
 export default app;
