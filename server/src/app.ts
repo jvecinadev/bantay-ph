c115df4720bc51da05583d0a5d624f89effe9a5f
VCNtech-123 <johnvecina640@gmail.com>
2026-09-10 03:57:39 +0800

    refactor: clean up code formatting and improve middleware order in app setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,20 +3,27 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import helmet from 'helmet'
 import { errorHandler } from './middleware/errorHandler.middleware'
-import routes from "./routes"
+import routes from './routes'
+import { rateLimiter } from './middleware/rateLimiter.middleware'
 
 const app = express()
 
-app.use(express.json())
-app.set("trust proxy", 1)
-app.use(cors({
-        origin: 'http://localhost:5173',
-        credentials: true
-}))
-app.use(cookieParser())
+app.set('trust proxy', 1)
+
 app.use(helmet())
-app.use("/api", routes)
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}))
+
+app.use(cookieParser())
+
+app.use('/api', rateLimiter)
+app.use(express.json({ limit: '10kb' }))
+app.use(express.urlencoded({ extended: true, limit: '10kb' }))
+
+app.use('/api', routes)
 
 app.use(errorHandler)
 
-export default app;
+export default app
