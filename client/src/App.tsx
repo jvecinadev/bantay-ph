2ca94fe23e6b9898f2a39515513e19950efbe983
VCNtech-123 <johnvecina640@gmail.com>
2026-09-12 03:55:03 +0800

    feat: restructure app component and routing; create routes configuration

--- client/src/App.tsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-  return (
-    <div className="flex h-screen items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold text-blue-400 underline">
-        React + Tailwind CSS is working!
-      </h1>
-    </div>
-  );
-}
