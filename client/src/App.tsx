a681a7174a76c773a48578145f5d887dfd534820
VCNtech-123 <johnvecina640@gmail.com>
2026-09-11 23:05:49 +0800

    feat: add axios and react-router-dom dependencies; implement basic App component

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold text-blue-400 underline">
+        React + Tailwind CSS is working!
+      </h1>
+    </div>
+  );
+}
