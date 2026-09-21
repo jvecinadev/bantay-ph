e679ab7a5ed3875fa4d51b81bbad4cfe77ad94b5
jvecinadev <johnvecina640@gmail.com>
2026-09-21 14:28:01 +0800

    feat: integrate Cloudinary for image storage and add environment variables

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -4,5 +4,8 @@ export const env = {
     jwt: process.env.JWT_SECRET as string,
     expdate: process.env.JWT_EXPIRES_IN as string,
     authCookieName: process.env.COOKIE_NAME as string,
-    clientUrl: process.env.CLIENT_URL as string
+    clientUrl: process.env.CLIENT_URL as string,
+    cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME as string,
+    cloudinaryApiKey: process.env.CLOUDINARY_API_KEY as string,
+    cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET as string
 }
